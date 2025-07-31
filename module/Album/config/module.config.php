4bc52afc967a1e4a2146a7f96813362899fa2dba
Erik Avelino <erikrocha98.jobs@gmail.com>
2025-07-31 13:29:45 -0300

    feat: implementa esqueleto controller e view

--- module/Album/config/module.config.php
+++ module/Album/config/module.config.php
@@ -1,5 +1,8 @@
+<?php
+
 namespace Album;
 
+use Zend\Router\Http\Segment;
 use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
@@ -8,6 +11,27 @@ return [
             Controller\AlbumController::class => InvokableFactory::class,
         ],
     ],
+
+    //Configuração das rotas da aplicação
+    'router' => [
+        'routes' => [
+            'album' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/album[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\AlbumController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+
     'view_manager' => [
         'template_path_stack' => [
             'album' => __DIR__ . '/../view',
